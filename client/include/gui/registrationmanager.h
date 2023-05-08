1975301803ced720418699fa570212cc159e3249
ubuntu <nik.chujko@mail.ru>
2023-05-08 13:31:48 +0300

    Add sign_up

--- client/include/gui/registrationmanager.h
+++ client/include/gui/registrationmanager.h
@@ -3,18 +3,53 @@
 #define REGISTRATIONMANAGER_H
 
 #include "user.h"
+#include <QRegularExpression>
 
-
-class RegistrationManager
-{
+class RegistrationManager {
 public:
-    RegistrationManager();
+  RegistrationManager();
+  /*
+@brief Функция создания пользователя
+@detailed Данная функция формирует обьект пользователь
+@param name Имя пользователя
+@param email Почта
+@param password Пароль
+@return Возвращает обьект user который становится текущим и записывается в базу
+данных
+*/
+  User registerUser(const QString &name, const QString &email,
+                    const QString &password);
+  /*
+@brief Функция для проверки корректности полей при создании пользователя
+@detailed Данная функция проверяет почту на присутствие символа @ и пароль на
+длину, регистры и символы
+@param name Имя пользователя
+@param email Почта
+@param password Пароль
+@return Возвращает true or false в зависимости от того, соответствуют ли данные
+регистрации требованиям к безопасности
+*/
+  bool validateUserDetails(const QString &name, const QString &email,
+                           const QString &password);
 
-    User registerUser(const QString& name, const QString& email, const QString& password);
-
-    bool validateUserDetails(const User& user);
-
-    bool saveUserToDatabase(const User& user);
+  bool saveUserToDatabase(const User &user);
+  /*
+@brief Функция для проверки корректности имени пользователя
+@detailed Данная функция проверяет есть ли в базе такой же username
+@param name Имя пользователя
+@return Возвращает true если имя уникальное, false если такой пользователь уже
+есть
+*/
+  bool isUsernameAvailable(const QString &username);
+  /*
+@brief Функция для проверки корректности почты
+@detailed Данная функция проверяет соответствует ли строка формату почты
+@param email Почта
+@return Возвращает true если соответствует, иначе false
+*/
+  bool isEmailValid(const QString &email);
+  bool isPasswordValid(const QString &password, const QString &username,
+                       const QString &email);
 };
 
 #endif // REGISTRATIONMANAGER_H
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,13 +1,54 @@
 //
 // Created by Алексей on 01.05.2023.
 //
+//#include "crypto.h"
 #include "mainwindow.h"
+#include "registrationmanager.h"
 #include <QApplication>
+#include <QDebug>
 #include <iostream>
-
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow w;
+  RegistrationManager r;
+  QString email = "user@user.com";
+  if (r.isEmailValid(email)) {
+    qDebug() << "YES email"
+             << "\n";
+  } else {
+    qDebug() << "NO"
+             << "\n";
+  }
+
+  QString pas1 = "et45hjdtj@$grsgFFF!";
+  QString pas2 = "1234";
+  QString pas3 = "user";
+
+  QString username = "user";
+
+  if (r.isPasswordValid(pas1, username, email)) {
+    qDebug() << "YES pas1"
+             << "\n";
+  } else {
+    qDebug() << "NO pas1"
+             << "\n";
+  }
+
+  if (r.isPasswordValid(pas2, username, email)) {
+    qDebug() << "YES pas2"
+             << "\n";
+  } else {
+    qDebug() << "NO pas2"
+             << "\n";
+  }
+
+  if (r.isPasswordValid(pas3, username, email)) {
+    qDebug() << "YES pas3"
+             << "\n";
+  } else {
+    qDebug() << "NO pas3"
+             << "\n";
+  }
 
   w.show();
   return a.exec();
