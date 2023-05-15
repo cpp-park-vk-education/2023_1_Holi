66a5a370670c4dbe740e480973fda5d12ea1b518
ShelbyKS <krylov.sanches@mail.ru>
2023-05-15 20:54:09 +0300

    update

---
+++ client/build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/build/client_autogen/mocs_compilation.cpp" "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/api_client/VK/vk_client.cpp" "CMakeFiles/client.dir/src/api_client/VK/vk_client.cpp.o" "gcc" "CMakeFiles/client.dir/src/api_client/VK/vk_client.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/api_client/VK/vk_request.cpp" "CMakeFiles/client.dir/src/api_client/VK/vk_request.cpp.o" "gcc" "CMakeFiles/client.dir/src/api_client/VK/vk_request.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/api_client/VK/vk_response.cpp" "CMakeFiles/client.dir/src/api_client/VK/vk_response.cpp.o" "gcc" "CMakeFiles/client.dir/src/api_client/VK/vk_response.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/api_client/YT/yt_client.cpp" "CMakeFiles/client.dir/src/api_client/YT/yt_client.cpp.o" "gcc" "CMakeFiles/client.dir/src/api_client/YT/yt_client.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/api_client/YT/yt_request.cpp" "CMakeFiles/client.dir/src/api_client/YT/yt_request.cpp.o" "gcc" "CMakeFiles/client.dir/src/api_client/YT/yt_request.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/api_client/YT/yt_response.cpp" "CMakeFiles/client.dir/src/api_client/YT/yt_response.cpp.o" "gcc" "CMakeFiles/client.dir/src/api_client/YT/yt_response.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/albums.cpp" "CMakeFiles/client.dir/src/gui/albums.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/albums.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/authenticationmanager.cpp" "CMakeFiles/client.dir/src/gui/authenticationmanager.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/authenticationmanager.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/chat.cpp" "CMakeFiles/client.dir/src/gui/chat.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/chat.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/config.cpp" "CMakeFiles/client.dir/src/gui/config.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/config.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/friends.cpp" "CMakeFiles/client.dir/src/gui/friends.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/friends.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/friends_in.cpp" "CMakeFiles/client.dir/src/gui/friends_in.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/friends_in.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/friends_out.cpp" "CMakeFiles/client.dir/src/gui/friends_out.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/friends_out.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/login.cpp" "CMakeFiles/client.dir/src/gui/login.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/login.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/main.cpp" "CMakeFiles/client.dir/src/gui/main.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/main.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/main_page.cpp" "CMakeFiles/client.dir/src/gui/main_page.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/main_page.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/mainwindow.cpp" "CMakeFiles/client.dir/src/gui/mainwindow.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/mainwindow.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/oauthprovider.cpp" "CMakeFiles/client.dir/src/gui/oauthprovider.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/oauthprovider.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/onvk.cpp" "CMakeFiles/client.dir/src/gui/onvk.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/onvk.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/onyoutube.cpp" "CMakeFiles/client.dir/src/gui/onyoutube.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/onyoutube.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/registrationmanager.cpp" "CMakeFiles/client.dir/src/gui/registrationmanager.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/registrationmanager.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/requestmaker.cpp" "CMakeFiles/client.dir/src/gui/requestmaker.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/requestmaker.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/signup.cpp" "CMakeFiles/client.dir/src/gui/signup.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/signup.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/gui/user.cpp" "CMakeFiles/client.dir/src/gui/user.cpp.o" "gcc" "CMakeFiles/client.dir/src/gui/user.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/main.cpp" "CMakeFiles/client.dir/src/main.cpp.o" "gcc" "CMakeFiles/client.dir/src/main.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/request_maker/message_info.cpp" "CMakeFiles/client.dir/src/request_maker/message_info.cpp.o" "gcc" "CMakeFiles/client.dir/src/request_maker/message_info.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/request_maker/response_handler.cpp" "CMakeFiles/client.dir/src/request_maker/response_handler.cpp.o" "gcc" "CMakeFiles/client.dir/src/request_maker/response_handler.cpp.o.d"
+  "/Users/shelby/Documents/VK/C++/project/2023_1_Holi/client/src/user/user.cpp" "CMakeFiles/client.dir/src/user/user.cpp.o" "gcc" "CMakeFiles/client.dir/src/user/user.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ client/build/client_autogen/BMPI5EA7AM/moc_mainwindow.cpp
@@ -0,0 +1,209 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../include/gui/mainwindow.h"
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_MainWindow_t {
+    uint offsetsAndSizes[26];
+    char stringdata0[11];
+    char stringdata1[23];
+    char stringdata2[1];
+    char stringdata3[23];
+    char stringdata4[24];
+    char stringdata5[23];
+    char stringdata6[25];
+    char stringdata7[26];
+    char stringdata8[21];
+    char stringdata9[21];
+    char stringdata10[25];
+    char stringdata11[24];
+    char stringdata12[25];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_MainWindow_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 22),  // "on_main_button_clicked"
+        QT_MOC_LITERAL(34, 0),  // ""
+        QT_MOC_LITERAL(35, 22),  // "on_chat_Button_clicked"
+        QT_MOC_LITERAL(58, 23),  // "on_AlbomsButton_clicked"
+        QT_MOC_LITERAL(82, 22),  // "on_Friends_bar_clicked"
+        QT_MOC_LITERAL(105, 24),  // "on_input_friends_clicked"
+        QT_MOC_LITERAL(130, 25),  // "on_output_friends_clicked"
+        QT_MOC_LITERAL(156, 20),  // "on_VK_button_clicked"
+        QT_MOC_LITERAL(177, 20),  // "on_YT_Button_clicked"
+        QT_MOC_LITERAL(198, 24),  // "on_Config_button_clicked"
+        QT_MOC_LITERAL(223, 23),  // "on_button_login_clicked"
+        QT_MOC_LITERAL(247, 24)   // "on_signUp_button_clicked"
+    },
+    "MainWindow",
+    "on_main_button_clicked",
+    "",
+    "on_chat_Button_clicked",
+    "on_AlbomsButton_clicked",
+    "on_Friends_bar_clicked",
+    "on_input_friends_clicked",
+    "on_output_friends_clicked",
+    "on_VK_button_clicked",
+    "on_YT_Button_clicked",
+    "on_Config_button_clicked",
+    "on_button_login_clicked",
+    "on_signUp_button_clicked"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      11,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   80,    2, 0x08,    1 /* Private */,
+       3,    0,   81,    2, 0x08,    2 /* Private */,
+       4,    0,   82,    2, 0x08,    3 /* Private */,
+       5,    0,   83,    2, 0x08,    4 /* Private */,
+       6,    0,   84,    2, 0x08,    5 /* Private */,
+       7,    0,   85,    2, 0x08,    6 /* Private */,
+       8,    0,   86,    2, 0x08,    7 /* Private */,
+       9,    0,   87,    2, 0x08,    8 /* Private */,
+      10,    0,   88,    2, 0x08,    9 /* Private */,
+      11,    0,   89,    2, 0x08,   10 /* Private */,
+      12,    0,   90,    2, 0x08,   11 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSizes,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'on_main_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_chat_Button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_AlbomsButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Friends_bar_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_input_friends_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_output_friends_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_VK_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_YT_Button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Config_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_button_login_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_signUp_button_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_main_button_clicked(); break;
+        case 1: _t->on_chat_Button_clicked(); break;
+        case 2: _t->on_AlbomsButton_clicked(); break;
+        case 3: _t->on_Friends_bar_clicked(); break;
+        case 4: _t->on_input_friends_clicked(); break;
+        case 5: _t->on_output_friends_clicked(); break;
+        case 6: _t->on_VK_button_clicked(); break;
+        case 7: _t->on_YT_Button_clicked(); break;
+        case 8: _t->on_Config_button_clicked(); break;
+        case 9: _t->on_button_login_clicked(); break;
+        case 10: _t->on_signUp_button_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 11)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 11;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 11)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 11;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
