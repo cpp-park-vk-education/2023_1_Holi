64d35399d87f904c53b24883cf971fc751eb3aed
ShelbyKS <krylov.sanches@mail.ru>
2023-05-15 20:59:05 +0300

    deleted cache

--- client/include/api_client/VK/vk_request.hpp
+++ client/include/api_client/VK/vk_request.hpp
@@ -1,4 +1,7 @@
+#pragma once
+
 #include "request.hpp"
+#include "requestmaker_https.hpp"
 #include <memory>
 
 class VKRequest : public Request {
--- client/include/api_client/VK/vk_response.hpp
+++ client/include/api_client/VK/vk_response.hpp
@@ -1,5 +1,7 @@
 #include "response.hpp"
 
+//constexpr std::map<>
+
 class VKResponse: public Response{
 public:
     VKResponse(){}
