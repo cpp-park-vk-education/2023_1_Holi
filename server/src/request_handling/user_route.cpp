8dff914664898b8348cbde973e5c32a28f5fbe58
Alexey <ax.chinaev@yandex.ru>
2023-05-16 12:37:35 +0300

    fix user route post

--- server/src/request_handling/user_route.cpp
+++ server/src/request_handling/user_route.cpp
@@ -6,41 +6,41 @@
 
 
 MessageInfo UserRoute::Get(int id) {
-//    if (id <= 0) {
-//        std::cerr << "Negative id" << std::endl;
-//        return {{}, http::status::not_found};
-//    }
-//
-//    std::string sql = R"(select * from "User" where id = )" + std::to_string(id);
-//
-//    QSqlQuery query;
-//    try {
-//        query = QSqlQuery(db_connector_->Connect());
-//        query.prepare(sql.c_str());
-//        query = db_connector_->MakeQuery(std::move(query));
-//    } catch (ConditionError &e) {
-//        std::cerr << e.what() << std::endl;
-//        return {{}, http::status::internal_server_error};
-//    } catch (...) {
-//        std::cerr << "Unexpected error with DB" << std::endl;
-//        return {{}, http::status::internal_server_error};
-//    }
-//
-//    std::vector<std::string> keys{"id", "name", "surname", "email", "login", "password"};
-//    json::object init;
-//    while (query.next()) {
-//        for (const auto &i: keys) {
-//            init[i] = query.value(i.c_str()).toString().toStdString();
-//        }
-//    }
-//
-//    json::value body(init);
-//    std::cout << "\t\t--- Create json value" << std::endl;
-//
-//    return {body, http::status::ok};
-//}
-//
-//MessageInfo UserRoute::Post(json::value body) {
+    if (id <= 0) {
+        std::cerr << "Negative id" << std::endl;
+        return {{}, http::status::not_found};
+    }
+
+    std::string sql = R"(select * from "User" where id = )" + std::to_string(id);
+
+    QSqlQuery query;
+    try {
+        query = QSqlQuery(db_connector_->Connect());
+        query.prepare(sql.c_str());
+        query = db_connector_->MakeQuery(std::move(query));
+    } catch (ConditionError &e) {
+        std::cerr << e.what() << std::endl;
+        return {{}, http::status::internal_server_error};
+    } catch (...) {
+        std::cerr << "Unexpected error with DB" << std::endl;
+        return {{}, http::status::internal_server_error};
+    }
+
+    std::vector<std::string> keys{"id", "name", "surname", "email", "login", "password"};
+    json::object init;
+    while (query.next()) {
+        for (const auto &i: keys) {
+            init[i] = query.value(i.c_str()).toString().toStdString();
+        }
+    }
+
+    json::value body(init);
+    std::cout << "\t\t--- Create json value" << std::endl;
+
+    return {body, http::status::ok};
+}
+
+MessageInfo UserRoute::Post(json::value body) {
 //    if (user_id_ <= 0) {
 //        std::cerr << "Non-positive  id" << std::endl;
 //        return {{}, http::status::not_found};
@@ -112,6 +112,7 @@ MessageInfo UserRoute::Get(int id) {
 //    std::cout << "\t\t--- Create json value" << std::endl;
 //
 //    return {response_body, http::status::ok};
+    return {};
 }
 
 MessageInfo UserRoute::Delete(int id) {
